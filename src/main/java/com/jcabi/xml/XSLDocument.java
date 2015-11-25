6b4806a960bd1a632cca3cfcd5678bd3cf91dfec
imrandurranee <imrandurranee@hotmail.com>
2015-11-25 10:57:54 -0500

    Update XSLDocument.java
    Added new Constructor with Error Listener

--- src/main/java/com/jcabi/xml/XSLDocument.java
+++ src/main/java/com/jcabi/xml/XSLDocument.java
@@ -107,7 +107,7 @@ public final class XSLDocument implements XSL {
     /**
      * Error listener.
      */
-    private static final ErrorListener ERRORS = new ErrorListener() {
+    private final transient ErrorListener ERRORS = new ErrorListener() {
         @Override
         public void warning(final TransformerException exception) {
             Logger.warn(this, exception.getMessageAndLocation());
@@ -174,6 +174,16 @@ public final class XSLDocument implements XSL {
     public XSLDocument(@NotNull(message = "XSL input stream can't be NULL")
         final InputStream stream) {
         this(new TextResource(stream).toString());
+    }
+
+    /**
+     * Public ctor, from XSL as a string with error listener.
+     * @param src XML document body
+     * @param elistener Error Listener
+     */
+    public XSLDocument(final String src, ErrorListener elistener) {
+        this(src, Sources.DUMMY);
+        this.ERRORS = elistener;
     }
 
     /**
